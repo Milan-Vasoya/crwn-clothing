d802857d3e275ff606dd8e1ffe061d48e681001c
Milan-Vasoya <milanvasoya231@gmail.com>
2021-01-20 09:54:37 +0530

    intial crwn clothing

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './Homepage.component';
+import '../styles/styles.scss';
+
+const App=()=> {
+  return (
+    <div>
+     <HomePage />
+    </div>
+  );
+}
+
+export {App as default};
