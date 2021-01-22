890859f6a184ab7286fbb034f7057a982606af11
Milan-Vasoya <milanvasoya231@gmail.com>
2021-01-22 18:29:46 +0530

    adding items to cart dropdown with styling

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./cart.dropdown.styles.scss";
+import {  connect} from "react-redux";
+import CartItem from "../cart-item/cari-iteam.component";
 
-const CartDropDown = ()=>(
+const CartDropDown = ({cartItems})=>(
     <div className='cart-dropdown '>
     <div className='cart-items'>
-    
+    {
+        cartItems.map((item)=><CartItem key={item.id} item={item} />)
+    }
     </div>
     <button className='black-button'>GO To CHECKOUT</button>
     </div>
 )
-export default CartDropDown;
+
+
+const mapStateToProps = ({cart: {cartItems}})=> ({
+    cartItems
+});
+export default connect(mapStateToProps)(CartDropDown);
