d5026f42ab28e7562b7c35f0521f22e685ed21b7
Milan-Vasoya <milanvasoya231@gmail.com>
2021-01-25 18:29:53 +0530

    add payment stripe front-end

--- src/components/checkout/CheckOut.componet.jsx
+++ src/components/checkout/CheckOut.componet.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./CheckOut.styles.scss";
 import { connect } from "react-redux";
 import CheckoutItem from "../checkout-item/CheckOut-Item.component";
+import StripeButton from "../stripe-button/stripe-button.component";
 import { createStructuredSelector } from "reselect";
 import {
   selectCartItems,
@@ -36,6 +37,12 @@ const CheckOut = ({ cartItems, total }) => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className="total">TOTAL: ₹{total}</div>
+      <div className='test-warning'>
+      * please use this creditcard information  for any payments (visa card)
+      <br />
+      4242 4242 4242 4242 - EXP: 01/22 - CVV: 123
+      </div>
+      <StripeButton price={total} />
     </div>
   );
 };
---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import StripeCheckOut from 'react-stripe-checkout';
+
+const StripCheckOutButton=({price})=>{
+    const publishableKey= 'pk_test_51IDTC1JecPfqHVXr73ZXs1r2tiiBbybsAF5645AJanLIFuOOWON0fWvatWB4fMx99OcRPgtSL8jv9rKBAPpoSIbR00wp0StntH';
+
+    const onsTokenSubmission=()=>alert('payment Sucessful')
+    return(
+        <StripeCheckOut
+        label='Pay Now'
+        name='crown Clothing'
+        billingAddress
+        shippingAddress
+        image='https://sendeyo.com/up/d/f3eb2117da'
+        description={`Your total is ₹${price}`}
+        amount={price}
+        panelLabel='pay now !'
+        token={onsTokenSubmission}
+       stripeKey={publishableKey}
+        />
+    );
+}
+
+export default StripCheckOutButton;
