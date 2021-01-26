28c8e81ad6adfb1d59eda2aaf22ac821dfc96813
Milan-Vasoya <milanvasoya231@gmail.com>
2021-01-26 10:41:52 +0530

    production mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,10 @@ import logger from 'redux-logger';
 import RootReducer from './Root-Reducer.reducer';
 import {  persistStore} from 'redux-persist';
 
-const MiddleWare = [logger];
+const MiddleWare = [];
+if(process.env.NODE_ENV==='development'){
+    MiddleWare.push(logger);
+}
 
 export const store = createStore(RootReducer, applyMiddleware(...MiddleWare));
 
