7727f688fc8f6fdb9f1860bc3fb15e4874136ad8
Milan-Vasoya <milanvasoya231@gmail.com>
2021-01-29 18:08:25 +0530

    Seprating our crn app into client Folder, added express server to help handle back end stripr payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import StripeCheckOut from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripCheckOutButton=({price})=>{
     const publishableKey= 'pk_test_51IDTC1JecPfqHVXr73ZXs1r2tiiBbybsAF5645AJanLIFuOOWON0fWvatWB4fMx99OcRPgtSL8jv9rKBAPpoSIbR00wp0StntH';
-
-    const onsTokenSubmission=()=>alert('payment Sucessful')
+    const priceForStripe = price*100;
+    const onsTokenSubmission=(token)=>{
+        axios({
+            url:'payment',
+            method:'post',
+            data:{
+                amount:priceForStripe,
+                token
+            }
+        }).then(response=>{
+            alert('payment is successful')
+        }).catch((error)=>{
+            console.log('request stripe Error', JSON.parse(error))
+            alert('there was an issue with your payment, Please make sure you use provided credit card');
+        })
+    }
     return(
         <StripeCheckOut
         label='Pay Now'
@@ -12,10 +27,10 @@ const StripCheckOutButton=({price})=>{
         billingAddress
         shippingAddress
         image='https://sendeyo.com/up/d/f3eb2117da'
-        currency='INR'
         description={`Your total is ₹${price}`}
-        amount={price*100}
+        amount={priceForStripe}
         panelLabel='pay now !'
+        currency='INR'
         token={onsTokenSubmission}
        stripeKey={publishableKey}
         />
